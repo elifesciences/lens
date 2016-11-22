165d276b183a3ebd5c86bad866c0cbb7ee5398cb
Michael Aufreiter <michael@substance.io>
2016-11-22 19:14:16 +0100

    Improved date formatting.

--- article/article_util.js
+++ article/article_util.js
@@ -21,7 +21,7 @@ util.formatDate = function (pubDate) {
     // new Date(year, month [, day [, hours[, minutes[, seconds[, ms]]]]])
     // Note: months are 0-based
     var localDate = new Date(parts[0], parts[1]-1, parts[2]);
-    return localDate.toUTCString().slice(0, 16);
+    return localDate.toDateString().slice(4, 16)
   } else if (parts.length === 2) {
     var month = parts[1].replace(/^0/, "");
     var year = parts[0];
